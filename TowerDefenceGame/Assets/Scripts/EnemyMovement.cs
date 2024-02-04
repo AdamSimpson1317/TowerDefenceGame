a85b82dab2c299fb8dee08d60eed750bdb4037d5
AdamSimpson1317 <98826882+AdamSimpson1317@users.noreply.github.com>
2024-02-04 16:12:35 +0000

    Enemy moves along track

---
+++ TowerDefenceGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    //Move from 1 point to another
+    //Once at the end trigger the health script and Destroy object
+
+    public GameObject[] corners;
+    public float speed = 1f;
+    public GameObject target;
+    public int counter = 1;
+
+    private void Start()
+    {
+        target = corners[0];
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, target.transform.position) > 0.1f) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
+        
+        else if(Vector3.Distance(transform.position, target.transform.position) < 0.1f)
+        {
+            target = corners[counter];
+            counter++;
+        }
+
+    }
+
+}
