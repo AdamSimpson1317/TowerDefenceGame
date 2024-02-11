54130d17282aa3fa31dd3ad713208513c0818517
Cursed-Soldier <98825272+Cursed-Soldier@users.noreply.github.com>
2024-02-11 17:23:28 +0000

    Removed empty start func

--- TowerDefenceGame/Assets/Scripts/TowerPlacement.cs
+++ TowerDefenceGame/Assets/Scripts/TowerPlacement.cs
@@ -12,12 +12,6 @@ public class TowerPlacement : MonoBehaviour
 
     public bool placing = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
