3d8a8e98885b93504194bd3aeb8e9343e49087a0
Cursed-Soldier <98825272+Cursed-Soldier@users.noreply.github.com>
2024-02-18 16:18:12 +0000

    Added tower placement menu with number control
    Towers placed cost gold now and remove it from world money

--- TowerDefenceGame/Assets/Scripts/TowerPlacement.cs
+++ TowerDefenceGame/Assets/Scripts/TowerPlacement.cs
@@ -9,6 +9,9 @@ public class TowerPlacement : MonoBehaviour
     public Tilemap tilemap;
     public Vector3Int location;
     public GameObject[] towerPrefabs;
+    public TowerType selectedTower;
+    public int selectedTowerCost;
+    public WorldMoney worldMoney;
 
     public bool placing = false;
 
@@ -17,15 +20,56 @@ public class TowerPlacement : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && !placing)
         {
-            StartCoroutine(TilePlace());
+            StartCoroutine(TilePlace(selectedTower));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetTowerType(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetTowerType(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetTowerType(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetTowerType(0);
         }
     }
-
-    IEnumerator TilePlace()
+    public void SetTowerType(int index)
     {
-        Debug.Log("placing");
+        if(index == 1)
+        {
+            selectedTower = TowerType.Archer;
+            selectedTowerCost = 100;
+        }
+        else if (index == 2)
+        {
+            selectedTower = TowerType.Infantry;
+            selectedTowerCost = 200;
+        }
+        else if (index == 3)
+        {
+            selectedTower = TowerType.Wizard;
+            selectedTowerCost = 300;
+        }
+        else
+        {
+            selectedTower = TowerType.None;
+            selectedTowerCost = 0;
+        }
+
+        
+    }
+
+    IEnumerator TilePlace(TowerType tower)
+    {
         placing = true;
 
+        //Get location to place tower (needs fixing)
         Vector3 mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mp.x = Mathf.Round(mp.x * 2) / 2;
         mp.y = Mathf.Round(mp.y * 2) / 2;
@@ -36,23 +80,45 @@ public class TowerPlacement : MonoBehaviour
 
         if (tilemap.GetTile(location))
         {
-            Debug.Log(location.ToString() + " : " + mp.ToString());
-            Instantiate(towerPrefabs[0], pos, Quaternion.identity);
+            if (worldMoney.money >= selectedTowerCost)
+            {
+                if (tower == TowerType.Archer)
+                {
+                    Debug.Log("placing");
+                    Instantiate(towerPrefabs[0], pos, Quaternion.identity);
+                    worldMoney.UpdateMoney(-selectedTowerCost);
+                }
+                else if (tower == TowerType.Infantry)
+                {
+                    Instantiate(towerPrefabs[1], pos, Quaternion.identity);
+                    worldMoney.UpdateMoney(-selectedTowerCost);
+                }
+                else if (tower == TowerType.Wizard)
+                {
+                    Instantiate(towerPrefabs[2], pos, Quaternion.identity);
+                    worldMoney.UpdateMoney(-selectedTowerCost);
+                }
+                else
+                {
+                    Debug.Log("No tower selected");
+
+                }
+            }
+
+
+            yield return new WaitForSeconds(.5f);
+
+            placing = false;
         }
-        else
-        {
-            Debug.Log("not placed");
 
-        }
-
-                
-
-
-
-        
-
-        yield return new WaitForSeconds(.5f);
-
-        placing = false;
     }
+
+    
+}
+
+public enum TowerType{
+    None,
+    Archer,
+    Infantry,
+    Wizard
 }
