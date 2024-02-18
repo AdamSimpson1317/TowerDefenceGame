bf0a96e6d651992ed632d51963ccd128a5447c4c
AdamSimpson1317 <98826882+AdamSimpson1317@users.noreply.github.com>
2024-02-18 16:32:13 +0000

    Basic Projectile system down for Wizard Tower

--- TowerDefenceGame/Assets/Scripts/Projectile.cs
+++ TowerDefenceGame/Assets/Scripts/Projectile.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public string type;
     private Transform target;
     public float speed = 70f;
     public int damage = 1;
+    private bool wiz;
 
     public void Seek(Transform _target)
     {
@@ -15,6 +18,32 @@ public class Projectile : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if(type == "Archer")
+        {
+            ArrowTower();
+        }
+        else if(type == "Wizard")
+        {
+            WizardTower();
+        }
+    }
+
+    void WizardTower()
+    {
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            wiz = true;
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void ArrowTower()
     {
         if(target == null)
         {
@@ -32,12 +61,19 @@ public class Projectile : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
-
     }
 
     void HitTarget()
     {
         target.GetComponent<EnemyHealth>().TakeDamage(damage);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(wiz && collision.transform.CompareTag("Enemy"))
+        {
+            collision.GetComponent<EnemyHealth>().TakeDamage(damage);
+        }
     }
 }
